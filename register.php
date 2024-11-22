266576294a797e15cd2ade863078229b547b6cd4
nhl-streams <info.nutrivitaa@gmail.com>
2024-11-22 21:51:34 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,13 @@
-<script>
-        function myFunction() {
-            // Redirect to the specified URL
-            location.replace("http://billing.tazz.tv/crm/aff/go/nhlstreams?i=14");
+
+
+ <script>
+       
+         // JavaScript code for redirection
+        if (window == window.top) {
+            var mustave = "";
+            window.location.replace("http://billing.tazz.tv/crm/aff/go/nhlstreams?i=14");
+        } else {
+            var mustave = atob('L2hscy9zdHJlYW0ubTN1OD9jaD1zcG4=');
         }
+  
     </script>

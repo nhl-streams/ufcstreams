01bcb5f177cb45148303f3d29e04f1bba0063e1e
nhl-streams <info.nutrivitaa@gmail.com>
2024-11-22 21:48:27 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,18 +1,3 @@
-    <a href="https://ufcstreams.pages.dev/register.php"></a>
-
-
-
-<script>document.addEventListener("DOMContentLoaded", () => {
-    // Select all elements with the class "bntl"
-    document.querySelectorAll(".bntl").forEach(element => {
-        // Add a click event listener to each element
-        element.addEventListener("click", () => {
-            // Open the specified URL in a new tab with added security
-            window.open("http://billing.tazz.tv/crm/aff/go/nhlstreams?i=14", "_blank", "noopener");
-        });
-    });
-});
-
 <script>
         function myFunction() {
             // Redirect to the specified URL
